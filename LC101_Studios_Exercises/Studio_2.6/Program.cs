9b6f9ade8ee83d12599bc074b4c6018724d4e2af
Ian Walsh <soccerstar955@gmail.com>
2020-10-01 19:36:20 -0500

    studio_2.6 completed

---
+++ LC101_Studios_Exercises/Studio_2.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Studio_2._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
