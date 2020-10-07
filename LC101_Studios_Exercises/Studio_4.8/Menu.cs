84823031efa2756213555afdcf8dacd46b91f837
Ian Walsh <soccerstar955@gmail.com>
2020-10-06 20:07:38 -0500

    changed name

--- LC101_Studios_Exercises/Studio_4.8/Menu.cs
+++ LC101_Studios_Exercises/Studio_4.8/Menu.cs
@@ -7,11 +7,10 @@ namespace Studio_4._8
     class menu
     {
         private List<MenuItem> menuItems = new List<MenuItem>();
-        private bool newItem;
         private DateTime lastUpdated;
         public menu()
         {
-            newItem = true;
+      
             menuItems.Add(new MenuItem("spare ribs", 10.99, "ribs", "main course"));
             menuItems.Add(new MenuItem("chicken soup", 5000.25, "warm soup", "appetizer"));
             menuItems.Add(new MenuItem("chocolate cake", 0.99, "cold cake", "dessert"));
@@ -21,10 +20,9 @@ namespace Studio_4._8
         {
             foreach (MenuItem item in menuItems)
             {
-                if (newItem && item.Name == menuItems[menuItems.Count-1].Name)
+                if (item.Name == menuItems[menuItems.Count-1].Name)
                 {
                     Console.Write("NEW!!!!!!   \n");
-                    newItem = false;
                 }
                 Console.Write("Name: " + item.Name + "\nPrice: $" + item.Price + "\nDescriprion: "
                         + item.Desc + "\nCategory: " + item.Category + "\n\n\n");
@@ -34,7 +32,6 @@ namespace Studio_4._8
         public void addItem(string name, double price, string desc, string category)
         {
             menuItems.Add(new MenuItem(name, price, desc, category));
-            newItem = true;
             lastUpdated = DateTime.Now;
         }
     }
